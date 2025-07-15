75e4f816cf9eb623981f8fc48b4c4c0b72dbd0b5
snakee0101 <snakee0101@gmail.com>
2025-07-15 14:28:47 +0300

    install primevue components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
+import InputText from 'primevue/inputtext';
+
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +17,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
+
+app.component('Button', Button);
+app.component('InputText', InputText);
+
 
 app.mount('#app')
--- frontend/src/views/Form.vue
+++ frontend/src/views/Form.vue
@@ -1,8 +1,13 @@
 <script setup>
+import { ref } from 'vue'
+
+const value = ref('')
 </script>
 
 <template>
   <main>
     There will be the form itself
+    <Button>test</Button>
+    <InputText type="text" v-model="value" />
   </main>
 </template>
